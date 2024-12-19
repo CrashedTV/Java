41d74ea55285a0f1152d7c900927aa00b1476ba2
CrashedTV <ayush.xtha01@gmail.com>
2024-12-19 21:34:23 +0545

    While Loop Challenge

---
+++ ControlFlow/src/WhileLoopChallenge.java
@@ -0,0 +1,26 @@
+public class WhileLoopChallenge {
+    public static void main(String[] args) {
+        int start = 4;
+        int end = 20;
+        int count = 0;
+        int odd = 0;
+        while(start<=end && count < 5){
+            start++;
+            if(!isEvenNumber(start)){
+                odd++;
+                continue;
+            }
+            System.out.println(start + " is prime number.");
+            count++;
+        }
+        System.out.println("Count of Odd : " + odd);
+        System.out.println("Count of Even : " + count);
+    }
+    public static boolean isEvenNumber(int start){
+
+        if(start % 2 ==0){
+            return true;
+        }
+        return false;
+    }
+}
