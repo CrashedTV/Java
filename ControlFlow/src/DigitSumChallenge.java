c37d223de358abecf812c06a199b1b0d9bd907ff
CrashedTV <ayush.xtha01@gmail.com>
2024-12-19 21:59:59 +0545

    Digit Sum Challenge

---
+++ ControlFlow/src/DigitSumChallenge.java
@@ -0,0 +1,19 @@
+public class DigitSumChallenge {
+    public static void main(String[] args) {
+        int number = 987;
+        System.out.println("The sum of " + number + " is " + sumDigits(number));
+    }
+    public static int sumDigits(int number){
+        if(number < 0){
+            return -1 ;
+        }
+        int sum = 0;
+        while (number > 9){
+            sum += number % 10;
+            number = number / 10;
+
+        }
+        sum += number;
+        return sum;
+    }
+}
