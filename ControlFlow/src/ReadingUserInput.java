7a5b57fe4556df4ab3d01a461527009d50129f0d
CrashedTV <ayush.xtha01@gmail.com>
2024-12-30 20:17:58 +0545

    Reading User Input

--- ControlFlow/src/ReadingUserInput.java
+++ ControlFlow/src/ReadingUserInput.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class ReadingUserInput {
     public static void main(String[] args) {
         int currentYear = 2024;
-        String dateOfBirthString = "2003";
-        int dateOfBirth = Integer.parseInt(dateOfBirthString);
-        System.out.println("Age : " + (currentYear-dateOfBirth));
+
+        //System.out.println(getInputFromConsole(currentYear));
+        System.out.println(getInputFromScanner(currentYear));
+
+    }
+    public static String getInputFromConsole(int currentYear){
+        String name = System.console().readLine("Hi, What's your name?");
+        System.out.println("Hi " + name + ", Thanks for taking the course.");
+        String dateOfBirth = System.console().readLine("What year you were born?");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "You are " + age + " years old";
+    }
+    public static String getInputFromScanner(int currentYear) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hi, What's your name?");
+        String name = scan.nextLine();
+        System.out.println("Hi " + name + ", Thanks for taking course.");
+
+        System.out.println("What year you were born?");
+        String dateOfBirth = scan.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "You are " + age + " years old";
     }
 }
