3b3cbe593b621e5d88eff16e5e837b0a82afca65
CrashedTV <ayush.xtha01@gmail.com>
2024-12-30 21:09:29 +0545

    Reading Input User Challenge

---
+++ ControlFlow/src/RedingInputUserChalloenge.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class RedingInputUserChalloenge {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count = 1;
+        double sum  = 0;
+        while (count <= 5){
+            System.out.println("Enter the number (" + count+"): " );
+            String number = scan.nextLine();
+            try{
+                double num =Double.parseDouble(number);
+                sum += num;
+                count++;
+
+            }
+            catch (NumberFormatException e){
+                System.out.println("Only numbers allowed.");
+            }
+        }
+        System.out.println("The sum is " +sum);
+    }
+
+}
